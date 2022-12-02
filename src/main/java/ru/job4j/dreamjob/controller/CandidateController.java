e125ca28274725908f24d6e81676fbe9737bd21e
bogdanove <anoth3rx@gmail.com>
2022-12-02 21:30:01 +0300

    9. Обновление кода без перезагрузки сервера. [#504846]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateController.java
@@ -15,13 +15,13 @@ public class CandidateController {
     private final CandidateStore candidateStore = CandidateStore.instOf();
 
     @GetMapping("/candidates")
-    public String posts(Model model) {
+    public String candidates(Model model) {
         model.addAttribute("candidates", candidateStore.findAll());
         return "candidates";
     }
 
     @GetMapping("/formAddCandidate")
-    public String addPost(Model model) {
+    public String addCandidate(Model model) {
         model.addAttribute("candidate", new Candidate(0, "Заполните название", "Заполните описание", LocalDate.now()));
         return "addCandidate";
     }
