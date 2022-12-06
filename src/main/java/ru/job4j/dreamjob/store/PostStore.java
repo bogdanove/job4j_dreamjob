6e22088ee2a5f5db7435e4b3126101130300e32a
bogdanove <anoth3rx@gmail.com>
2022-12-06 18:36:47 +0300

    3. PostController.createPost. Создание вакансии. [#504849]

--- src/main/java/ru/job4j/dreamjob/store/PostStore.java
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostStore {
 
@@ -13,11 +14,13 @@ public class PostStore {
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
+    private final AtomicInteger id = new AtomicInteger();
+
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job", "Ты научишься использовать инструмент сборки Maven. "
+        posts.put(1, new Post(id.incrementAndGet(), "Junior Java Job", "Ты научишься использовать инструмент сборки Maven. "
                 + "Будешь писать модульные тесты и оформлять свой код", LocalDate.now()));
-        posts.put(2, new Post(2, "Middle Java Job", "В этом уровне ты создашь парсер вакансий популярного сайта", LocalDate.now()));
-        posts.put(3, new Post(3, "Senior Java Job", "Ты научишься использовать Java фреймворки, "
+        posts.put(2, new Post(id.incrementAndGet(), "Middle Java Job", "В этом уровне ты создашь парсер вакансий популярного сайта", LocalDate.now()));
+        posts.put(3, new Post(id.incrementAndGet(), "Senior Java Job", "Ты научишься использовать Java фреймворки, "
                 + "которые используют профессионалы каждый день", LocalDate.now()));
     }
 
@@ -30,6 +33,8 @@ public class PostStore {
     }
 
     public Post add(Post post) {
+        post.setId(id.incrementAndGet());
+        post.setCreated(LocalDate.now());
         return posts.put(post.getId(), post);
     }
 }
